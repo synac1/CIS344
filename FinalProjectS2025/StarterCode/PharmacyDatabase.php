6533fe75e31d67a231fa97e89000ef3a07b9fabb
Yanilda <synac1@users.noreply.github.com>
2025-04-07 14:28:52 -0400

    main files

---
+++ FinalProjectS2025/StarterCode/PharmacyDatabase.php
@@ -0,0 +1,65 @@
+<?php
+class PharmacyDatabase {
+    private $host = "localhost";
+    private $port = "3306";
+    private $database = "pharmacy_portaldb";
+    private $user = "root";
+    private $password = "YourPassword";
+    private $connection;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->database, $this->port);
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+        echo "Successfully connected to the database";
+    }
+
+    public function addPrescription($patientUserName, $medicationId, $dosageInstructions, $quantity)  {
+        $stmt = $this->connection->prepare(
+            "SELECT userId FROM Users WHERE userName = ? AND userType = 'patient'"
+        );
+        $stmt->bind_param("s", $patientUserName);
+        $stmt->execute();
+        $stmt->bind_result($patientId);
+        $stmt->fetch();
+        $stmt->close()
+        
+        if (patientId){
+            $stmt = $this->connection->prepare(
+                "INSERT INTO prescriptions (userId, medicationId, dosageInstructions, quantity) VALUES (?, ?, ?, ?)"
+            );
+            $stmt->bind_param("iisi", $patientId, $medicationId, $dosageInstructions, $quantity);
+            $stmt->execute();
+            $stmt->close();
+            echo "Prescription added successfully";
+        }else{
+            echo "failed to add prescription";
+        }
+      
+    }
+
+    public function getAllPrescriptions() {
+        $result = $this->connection->query("SELECT * FROM  Prescriptions join Medications on Prescriptions.medicationId= Medications.medicationId");
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    
+    public function MedicationInventory() {
+        /*
+        Complete this function to test the functionality of
+        MedicationInventoryView and implement it in the server
+        */
+        //Wrire code here
+    }
+
+    public function addUser($userName, $contactInfo, $userType) {
+     //Write Code here
+    }
+
+    //Add Other needed functions here
+}
+?>
---
+++ FinalProjectS2025/StarterCode/PharmacyServer.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'PharmacyDatabase.php';
+
+class PharmacyPortal {
+    private $db;
+
+    public function __construct() {
+        $this->db = new PharmacyDatabase();
+    }
+
+    public function handleRequest() {
+        $action = $_GET['action'] ?? 'home';
+
+        switch ($action) {
+            case 'addPrescription':
+                $this->addPrescription();
+                break;
+            case 'viewPrescription':
+                $this->viewPrescriptions();
+                break;
+            case 'viewInventory':
+                $this->viewInventory();
+                break;
+            case 'addUser':
+                $this->addUser();
+                break;
+            default:
+                $this->home();
+        }
+    }
+
+    private function home() {
+        include 'templates/home.php';
+    }
+
+    private function addPrescription() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $patientUserName = $_POST['patient_username'];
+            $medicationId= $_POST['medication_id'];
+            $dosageInstructions = $_POST['dossage_instructions'];
+            $quantity = $_POST['quantity'];
+
+            $this->db->addPrescription($patientUserName, $medicationId, $dosageInstructions, $quantity);
+            header("Location: index.php?action=viewReservations&message=Reservation Added");
+        } else {
+            include 'templates/addReservation.php';
+        }
+    }
+
+    private function viewPrescription() {
+        $reservations = $this->db->getAllPrescriptions();
+        include 'templates/viewPrescription.php';
+    }
+}
+
+$portal = new PharmacyPortal();
+$portal->handleRequest();
