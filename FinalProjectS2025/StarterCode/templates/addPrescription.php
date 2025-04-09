14243cfd76fcceca192c5d1842dec604365829d6
Yanilda <synac1@users.noreply.github.com>
2025-04-09 19:45:24 -0400

    Add files via upload

--- FinalProjectS2025/StarterCode/templates/addPrescription.php
+++ FinalProjectS2025/StarterCode/templates/addPrescription.php
@@ -1,14 +1,16 @@
 <html>
-<head><title>Add Prescription</title></head>
+<head><title>Add Prescription</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
 <body>
     <h1>Add Reservation</h1>
-    <form method="POST" action="index.php?action=addReservation">
-        Patient Username: <input type="text" name="patient_username"><br>
-        Medication ID : <input type="number" name="medication_id"><br>
-        Dossage Instructions: <textarea name="dossage_instructions"></textarea><br>
-        Quantity: <input type="number" name="quantity"><br>
+    <form method="POST" action="?action=addPrescription">
+        Patient Username: <input type="text" name="patient_username" /><br>
+        Medication ID : <input type="number" name="medication_id"/><br>
+        Dossage Instructions: <textarea name="dosage_instructions"></textarea><br>
+        Quantity: <input type="number" name="quantity" /><br>
         <button type="submit">Save</button>
     </form>
-    <a href="index.php">Back to Home</a>
+    <a href="PharmacyServer.php">Back to Home</a>
 </body>
 </html>
--- FinalProjectS2025/StarterCode/templates/home.php
+++ FinalProjectS2025/StarterCode/templates/home.php
@@ -1,8 +1,12 @@
 <html>
-<head><title>Restaurant Portal</title></head>
+<head><title>Pharmacy Portal</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
 <body>
-    <h1>Restaurant Portal</h1>
-    <a href="index.php?action=addPrescription">Add Prescription</a> |
-    <a href="index.php?action=viewPrescriptions">View Prescriptions</a>
+    <h1>Pharmacy Portal</h1>
+    <nav>
+        <a href="?action=addPrescription" class="nav-link">Add Prescription</a>
+        <a href="?action=viewPrescriptions" class="nav-link">View Prescriptions</a>
+    </nav>
 </body>
 </html>
